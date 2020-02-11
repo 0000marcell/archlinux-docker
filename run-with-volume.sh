2bdb709e1cb6fccde43c2d70e517bd6d683b19af
____marcell <0000marcell@gmail.com>
2020-02-11 15:59:33 +0000

    adding port forwarding to container

--- run-with-volume.sh
+++ run-with-volume.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-docker run -it -v ~/docker-volume:/root/docker-volume archlinux:1.0
+docker run -it -v ~/docker-volume:/root/docker-volume --publish 8000:8080 --name original archlinux:1.0
